f99530a57d842fbd8a238ff2f7471db2ca7d960e
pacosonTang <pacoson@163.com>
2015-09-29 20:20:46 +0800

    source of for chapter1

---
+++ chapter1/p6.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/* Âú×ãF(0)=0 ÇÒ F(X)=2F(X-1)+X^2 */
+int F(int);
+
+int main()
+{    
+    for(int i = 0; i <= 10; i++)
+        printf("F(%d) = %-4d\n",i,F(i));         
+}
+
+int F(int x)
+{
+    if(x == 0)
+        return 0;
+    else 
+		return 2 * F(x - 1) + x * x;
+}
