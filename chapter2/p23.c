9435ff860e9f8458b5e07604d24de28381b13c86
pacosonTang <pacoson@163.com>
2015-10-14 20:33:50 +0800

    computing the greatest common divisor

---
+++ chapter2/p23.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+#define ElementType int
+#define NOTFOUND -1
+
+/* 求两个数的最大公因数（greatest common divisor） */
+int gcd(int M, int N)
+{
+    int rem;
+
+    printf("remainder sequence: ");
+    while (N > 0)
+    {
+        rem = M % N;
+        M = N;
+        N = rem;
+        printf("%4d", rem);
+    }
+    return M;
+}
+
+main()
+{
+    int N = 1989, M = 1590;    
+	printf("\t\t\t ========== test for greatest common divisor ==========\n\n");
+    
+    int gcd_ = gcd(M, N);
+    printf("\nthe greatest common divisor between %4d and %4d is %4d\n", M, N, gcd_);
+
+}
+
+
+
+
