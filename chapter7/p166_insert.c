d9fc5b3864f1a63b712aca2cce9b8c17e89c1b6e
pacosonTang <pacoson@163.com>
2015-11-01 20:45:08 +0800

    implementation of executing inserting for the given array sort

---
+++ chapter7/p166_insert.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+// we adopt ascending order to execute inserting operation
+void insert(int data[], int size)
+{
+	int temp;
+	int p;
+	int index;
+
+	for(p = 1; p < size; p++)
+	{
+		index = p - 1;
+		temp = data[p];
+		while(index >= 0 && temp < data[index])
+		{
+			data[index+1] = data[index];
+			index--;
+		}
+		data[index+1] = temp;
+	}
+	
+}
+
+void printArray(int data[], int size)
+{
+	int i;
+
+	for(i = 0; i < size; i++)
+		printf("%5d", data[i]);
+	
+	printf("\n");
+}
+
+int main()
+{
+	int size;
+	int data[] = {100, 1000, 100, 10, 6, 2, 19, 25, 15};
+
+	size = 8;
+
+	printf("\n=== before executing inserting ===\n");
+	printArray(data, size);
+	insert(data, size);
+	printf("\n=== after executing inserting ===\n");
+	printArray(data, size);
+
+	return 0;
+}
