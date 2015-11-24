db3e9b4ca706515a6bebac5b99d921f14d21b457
pacosonTang <pacoson@163.com>
2015-11-24 09:54:52 +0800

    implementation of conducting dfs to traverse directed graph and judge whether it has circles or not

---
+++ chapter9/p248_dfs_directed_graph/adjTable.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <malloc.h>
+
+struct AdjTable;
+typedef struct AdjTable* AdjTable;
+
+struct AdjTable
+{
+	int vertex;
+	AdjTable next;	
+};
+
+AdjTable* initAdjTable(int size);
+AdjTable makeEmptyAdj();
+void printAdjTable(AdjTable* link, int size);
+void insertAdj(AdjTable*, int, int);
