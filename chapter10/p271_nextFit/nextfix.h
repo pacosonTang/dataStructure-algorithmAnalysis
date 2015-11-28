bdcdb7889a795299186d1631ab25caac23db8ded
pacosonTang <pacoson@163.com>
2015-11-28 10:52:55 +0800

    implementation of next fit to place item into boxes

---
+++ chapter10/p271_nextFit/nextfix.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <malloc.h>
+
+#define Error(str) printf("\n\t error: %s\n", str)
+#define BasicType double
+
+int size;
+struct Box;
+typedef struct Box Box;
+typedef struct Box* BoxPtr;
+
+struct Box
+{
+	double key;
+	BoxPtr next;
+};
+
+void initBox(int size);
+BoxPtr buildSingleElement();
+BasicType* buildBasicArray(int size, BasicType firstkey);
+void printBox(int);
+void nextfix(double key, int*);
+
+BoxPtr* boxes;
+double* surplus;
+
