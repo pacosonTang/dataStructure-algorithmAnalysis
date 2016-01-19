9a3b0f49c857638cdef1d43bf2535e5140f0ba97
pacosonTang <pacoson@163.com>
2016-01-19 15:42:50 +0800

    implementation for leftist heap with merge operation

---
+++ chapter6/p145_leftist_heap/leftist_heap.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <malloc.h>
+
+#define ElementType int
+struct TreeNode;
+#define Error(str) printf("\n error: %s \n",str) 
+typedef struct TreeNode* TreeNode;
+typedef struct TreeNode* PriorityQueue;
+
+struct TreeNode
+{
+	ElementType value;
+	TreeNode left;
+	TreeNode right;
+	int nullPathLen;
+};
+
+PriorityQueue insert(ElementType, PriorityQueue);
+PriorityQueue deleteMin(PriorityQueue);
+void printPreorder(int depth, TreeNode root);
+PriorityQueue merge(PriorityQueue, PriorityQueue);
+PriorityQueue innerMerge(PriorityQueue h1, PriorityQueue h2);
+void swap(PriorityQueue h1);
+int minimal(int a, int b);
