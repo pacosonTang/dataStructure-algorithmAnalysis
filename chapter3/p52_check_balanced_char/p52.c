1602dca0777832c79f070296712addbd1cceed4e
pacosonTang <pacoson@163.com>
2016-07-28 15:16:32 -0400

    realizing for check balanced char via application of stack

---
+++ chapter3/p52_check_balanced_char/p52.c
@@ -0,0 +1,48 @@
+#include "p52.h"
+
+int checkFile(Stack *s)
+{
+	FILE *fp;
+	ElementType c;
+	ElementType popc;
+
+	fp = fopen("D:\\classical_books\\datastructure_alg\\source_code\\chapter3\\p52\\temp.txt", "r");
+		
+	// only test for round bracket '()', bracket '[]', brace'{}'.
+	// do you know the meanings of open brace '{' and close brace '}'.
+	while((c=getc(fp)) != EOF)
+	{
+		if(c == '(' || c == '[' || c == '{')
+		{
+			push(s, c);
+		}
+		else if(c == ')' || c == ']' || c == '}')
+		{
+			pop(s, &popc);
+			if(c==')' && popc!= '(')
+			{
+				 return 0;
+			}
+			else if(c==']' && popc!= '[')
+			{
+				 return 0;
+			}
+			else if(c=='}' && popc!= '{')
+			{
+				 return 0;
+			}
+		}
+	}
+
+	return 1;
+}
+
+int main()
+{
+	Stack s;
+	int result;
+
+	initStack(&s, 1000);
+	result = checkFile(&s);
+	printf("%d", result);
+}
---
+++ chapter3/p52_check_balanced_char/p52.h
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <malloc.h>
+
+typedef char ElementType;
+
+typedef struct Stack
+{
+	int size;
+	char* head;
+	int top;
+}Stack;
+
+int isFull(Stack *s);
+int isEmpty(Stack *s);
+void initStack(Stack *s, int size);
+void push(Stack *s, char c);
+void pop(Stack *s, ElementType *e);
+
+int isFull(Stack *s)
+{
+	return s->size == s->top ? 1 : 0;
+}
+
+int isEmpty(Stack *s)
+{
+	return 0 == s->top ? 1 : 0;
+}
+
+void initStack(Stack *s, int size)
+{
+	s->size = size;
+	s->top = 0;
+	s->head = (ElementType*)malloc(sizeof(ElementType) * size);
+}
+
+void push(Stack *s, ElementType c)
+{
+	if(!isFull(s))
+	{
+		s->head[s->top++] = c;
+	} 
+	else
+	{
+		printf("%s", "failed pushing for stack is full.");
+	}
+}
+
+void pop(Stack *s, ElementType *e)
+{
+	if(!isEmpty(s))
+	{
+		*e = s->head[--s->top];
+	}
+	else
+	{
+		*e = ' ';
+		printf("%s", "failed poping for stack is empty.");
+	}	
+}
